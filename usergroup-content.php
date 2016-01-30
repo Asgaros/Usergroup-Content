c5ff876bb4c4ffaf0ea747468d8a549f56ece48c
Thomas <developer@thomasbelser.net>
2016-01-30 15:45:02 +0100

    Added plugin header information
    Added plugin header information.

---
+++ usergroup-content.php
@@ -0,0 +1,27 @@
+<?php
+/*
+  Plugin Name: Usergroup Content
+  Plugin URI: https://github.com/Asgaros/usergroup-content
+  Description: A lightweight and simple usergroup content plugin for WordPress.
+  Version: Dev
+  Author: xxx yyy
+  Author URI: http://xyz
+  License: GPL2
+  License URI: https://www.gnu.org/licenses/gpl-2.0.html
+  Text Domain: usergroup-content
+  Domain Path: /languages
+
+  Usergroup Content is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  any later version.
+
+  Usergroup Content is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Usergroup Content. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
+?>
